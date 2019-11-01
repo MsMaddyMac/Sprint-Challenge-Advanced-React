5d31e4e7060ee04f0fc050714482e88dbe429beb
Maddy <mmcintosh2k14@gmail.com>
2019-11-01 12:54:53 -0400

    deletes background image, adds axios call to app.js and creates players component

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
+// import Players from "./components/Players";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
-}
+
+
+class App extends Component {
+    state = {
+      players: [],
+    };
+  
+    componentDidMount() {
+      axios
+      .get('http://localhost:5000/api/players')
+      .then(res => {
+        console.log(res);
+        this.setState({
+          players: res.data
+        });
+      })
+      .catch(err => {
+        console.log("No players found!", err);
+      });
+    }
+
+    render() {
+      console.log('App is rendering...☺️')
+      return (
+        <div className="App">
+          
+        </div>
+      );
+    }
+  }
 
 export default App;
---
+++ client/src/components/Players.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const Players = props => {
+    return (
+        <div className="playerCard">
+
+        </div>
+    )
+}
